6acecbf7636c8055b06fc7687f98268cce37e6a3
sean <sean@sean.com>
2020-03-10 14:52:29 -0500

    Completed Activity 4

---
+++ activity-4/js/scripts.js
@@ -0,0 +1,17 @@
+// WELCOME MESSAGE
+var user = 'Sean';
+var salutation = 'Hi there, ';
+var greeting = salutation + user + "! Here are the latest Golden Retriever reviews.";
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+// PRODUCT PRICE
+var price = 20,
+    studentDiscount = .10,
+    studentPrice = price - (price*studentDiscount),
+    priceEl = document.getElementById('price'),
+    studentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+studentPriceEl.textContent = studentPrice.toFixed(2);
